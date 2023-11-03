c0f1fad273a0abdae04fe16fb82528f7216c7cb9
bibin <bibin@intuitiolabs.com>
2023-11-03 09:40:58 +0530

    updated the code to reflect bose assets

--- bose_asset.py
+++ bose_asset.py
@@ -11,8 +11,36 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Replace the URL with the one you want to scrape
-url = "https://www.boseprofessional.com/en_us/products/loudspeakers/line_arrays/arenamatch/arenamatch_am10.html#v=arenamatch_am10_60_black"
+def fetch_asset_urls(response):
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find all image tags (img) on the page
+    img_tags = soup.find_all('img')
+
+    # Extract image URLs
+    image_urls = []
+    other_urls =[]
+    for img in img_tags:
+        if 'data-srcset' in img.attrs:
+            image_urls.append(img['data-srcset'].split('jcr:content')[0])
+        elif 'src' in img.attrs:
+            if "assets.bose.com" in img['src']:
+               image_urls.append(img['src'])
+            else:
+                other_urls.append(img['src'])
+
+
+    # Find all elements with the specified class
+    elements = soup.find_all(class_="productDocuments")
+
+    # Extract href attributes
+    assets = []
+    for element in elements:
+        a_tags = element.find_all('a')
+        assets += [asset.get('href') for asset in a_tags if asset.get('href')]
+
+    return  image_urls, assets, other_urls
 
 bose_url = st.text_input("Input Bose Professional URL", None)
 
@@ -23,39 +51,20 @@ if bose_url:
 
     # Check if the request was successful
     if response.status_code == 200:
-        # Parse the HTML content of the page
-        soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Find all image tags (img) on the page
-        img_tags = soup.find_all('img')
+        img_urls , asset_urls, other_urls = fetch_asset_urls(response)
 
-        # Extract image URLs
-        image_urls = []
-        for img in img_tags:
-            if 'data-srcset' in img.attrs:
-                image_urls.append(img['data-srcset'].split('jcr:content')[0])
-            elif 'src' in img.attrs:
-                image_urls.append(img['src'])
-
-        # Find all elements with the specified class
-        elements = soup.find_all(class_="productDocuments")
-
-        # Extract href attributes
-        assets = []
-        for element in elements:
-            a_tags = element.find_all('a')
-            assets += [asset.get('href') for asset in a_tags if asset.get('href')]
 
         # Create separate DataFrames for image_urls and assets
-        df_images = pd.DataFrame({'Image URLs': image_urls})
-        df_assets = pd.DataFrame({'Asset HREFs': assets})
+        df_bose_images = pd.DataFrame({'Bynder URLs': img_urls})
+        df_assets = pd.DataFrame({'Asset URLS': asset_urls})
+        df_other_images= pd.DataFrame({'Other image URLS': other_urls})
 
         # Concatenate DataFrames along the columns (axis=1)
-        df_combined = pd.concat([df_images, df_assets], axis=1)
+        df_combined = pd.concat([df_bose_images, df_assets,df_other_images ], axis=1)
 
         # Print the first few rows of the combined DataFrame
         df_combined
-
 
 
     else:
@@ -63,4 +72,4 @@ if bose_url:
 
 
 
-    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
