a951eda4149b96b648c2c55300a7167556a00139
Shajaz Hashim <shajaz.hashim@gapac.com>
2023-11-08 11:08:29 +0530

    added other assets and label for related pages

--- bose_asset.py
+++ bose_asset.py
@@ -31,41 +31,58 @@ def fetch_asset_urls(response):
 
                image_urls.append(img['src'].replace("//assets","https://assets"))
             else:
-                other_urls.append(img['src'])
+                other_urls.append(img['src'].replace("//static","https://static"))
 
     # Find all elements with the specified class
     elements = soup.find_all(class_="productDocuments")
 
     # Extract href attributes
     assets = []
+    other_assets = []
     for element in elements:
         a_tags = element.find_all('a')
-        assets += [asset.get('href').replace("//assets","https://assets") for asset in a_tags if asset.get('href')]
+        for asset in a_tags:
+            if asset.get('href'):
+                if "assets.bose.com" in asset.get('href'):
+                    assets.append(asset.get('href').replace("//assets","https://assets"))
+                else:
+                    other_assets.append(asset.get('href'))
 
     elements = soup.find_all(class_="productList")
+    elements += soup.find_all(class_="productReference")
 
     # Extract href attributes
-    related_pages = []
+    related_labels_pages = {}
     for element in elements:
-        print(element)
+        # print(element)
         a_tags = element.find_all('a')
-        related_pages += ["https://www.boseprofessional.com"+asset.get('href') for asset in a_tags if asset.get('href') and "//assets" not in asset.get('href')]
-        related_pages = list(set(related_pages))
-    return  image_urls, assets, other_urls, related_pages
+        for asset in a_tags:
+            if asset.get('href') and "//assets" not in asset.get('href'):
+                related_labels_pages["https://www.boseprofessional.com"+asset.get('href')] = asset.text
+
+    related_pages = []
+    related_labels = []
+    for label in related_labels_pages:
+        related_labels.append(related_labels_pages[label])
+        related_pages.append(label)
+
+    return  image_urls, assets, other_urls, other_assets, related_pages, related_labels
 
 def scrape_data(page_url):
     response = requests.get(page_url)
 
     # Check if the request was successful
     if response.status_code == 200:
-        img_urls, asset_urls, other_urls, related_pages= fetch_asset_urls(response)
+        img_urls, asset_urls, other_urls, other_assets, related_pages, related_labels= fetch_asset_urls(response)
 
     df_bose_images = pd.DataFrame({'asset.bose image URLs': img_urls})
     df_assets = pd.DataFrame({'asset.bose asset URLS': asset_urls})
     df_other_images = pd.DataFrame({'Other image URLS': other_urls})
+    df_other_assets = pd.DataFrame({'Other asset URLS': other_assets})
     df_related_pages = pd.DataFrame({'Related pages': related_pages})
+    related_labels = pd.DataFrame({'Related labels': related_labels})
 
-    df_combined = pd.concat([df_bose_images, df_assets, df_other_images, df_related_pages], axis=1)
+    df_combined = pd.concat([df_bose_images, df_assets, df_other_images, df_other_assets, df_related_pages, related_labels], axis=1)
 
     return df_combined
 
@@ -88,9 +105,3 @@ if bose_url:
     df_combined =scrape_data(bose_url)
 
     df_combined
-
-
-
-
-
-
