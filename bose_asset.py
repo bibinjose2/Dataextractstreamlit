4a436acdc10093dd372016b0b630455cf3d0e909
Shajaz Hashim <shajaz.hashim@gapac.com>
2023-11-09 12:29:07 +0530

    bg image missing issue fixed

--- bose_asset.py
+++ bose_asset.py
@@ -18,7 +18,7 @@ def fetch_asset_urls(response):
     # Find all image tags (img) on the page
     main = soup.find_all('main')
     img_tags = main[0].find_all('img')
-    bg_image = main[0].find_all(class_='bose-story-block__backgroundContainer')
+    bg_image = main[0].find_all(class_='bose-story-block__backgroundContainer') + main[0].find_all(class_='bose-pageHeader__backgroundContainer')
     img_tags = bg_image + img_tags
 
     # Extract image URLs
@@ -27,7 +27,10 @@ def fetch_asset_urls(response):
     related_pages = []
     for img in img_tags:
         if 'data-bgset' in img.attrs:
-            image_urls.append(img['data-bgset'].split('320w')[0].replace("//assets","https://assets"))
+            if '160w' in img['data-bgset']:
+                image_urls.append(img['data-bgset'].split('160w')[0].replace("//assets","https://assets"))
+            else:
+                image_urls.append(img['data-bgset'].split('320w')[0].replace("//assets","https://assets"))
         if 'data-srcset' in img.attrs:
             image_urls.append(img['data-srcset'].split('320w')[0].replace("//assets","https://assets"))
 
@@ -45,14 +48,20 @@ def fetch_asset_urls(response):
     # Extract href attributes
     assets = []
     other_assets = []
+    asset_label = []
+    other_asset_label = []
     for element in elements:
         a_tags = element.find_all('a')
         for asset in a_tags:
             if asset.get('href'):
                 if "assets.bose.com" in asset.get('href'):
                     assets.append(asset.get('href').replace("//assets","https://assets"))
+                    label = asset.find_previous(class_='bose-list__title')
+                    asset_label.append(label.text if label else None)
                 else:
                     other_assets.append(asset.get('href'))
+                    label = asset.find_previous(class_='bose-list__title')
+                    other_asset_label.append(label.text if label else None)
 
     elements = soup.find_all(class_="productList")
     elements += soup.find_all(class_="productReference")
@@ -72,23 +81,25 @@ def fetch_asset_urls(response):
         related_labels.append(related_labels_pages[label])
         related_pages.append(label)
 
-    return  image_urls, assets, other_urls, other_assets, related_pages, related_labels
+    return  image_urls, assets, other_urls, other_assets, related_pages, related_labels, asset_label, other_asset_label
 
 def scrape_data(page_url):
     response = requests.get(page_url)
 
     # Check if the request was successful
     if response.status_code == 200:
-        img_urls, asset_urls, other_urls, other_assets, related_pages, related_labels= fetch_asset_urls(response)
+        img_urls, asset_urls, other_urls, other_assets, related_pages, related_labels, asset_label, other_asset_label= fetch_asset_urls(response)
 
-    df_bose_images = pd.DataFrame({'asset.bose image URLs': img_urls})
-    df_assets = pd.DataFrame({'asset.bose asset URLS': asset_urls})
+    df_bose_images = pd.DataFrame({'Bynder Image URLs': img_urls})
+    df_assets = pd.DataFrame({'Bynder Asset URLS': asset_urls})
+    df_assets_label = pd.DataFrame({'Bynder Asset Title': asset_label})
     df_other_images = pd.DataFrame({'Other image URLS': other_urls})
     df_other_assets = pd.DataFrame({'Other asset URLS': other_assets})
+    df_other_assets_label = pd.DataFrame({'Other Asset Title': other_asset_label})
     df_related_pages = pd.DataFrame({'Related pages': related_pages})
     related_labels = pd.DataFrame({'Related labels': related_labels})
 
-    df_combined = pd.concat([df_bose_images, df_assets, df_other_images, df_other_assets, df_related_pages, related_labels], axis=1)
+    df_combined = pd.concat([df_bose_images, df_assets, df_assets_label, df_other_images, df_other_assets, df_other_assets_label, df_related_pages, related_labels], axis=1)
 
     return df_combined
 
