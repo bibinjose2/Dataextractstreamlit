2b1393116b8000887910e9abb0e4439881beaf4b
Shajaz Hashim <shajaz.hashim@gapac.com>
2023-11-14 23:13:37 +0530

    tech info asset added

--- bose_asset.py
+++ bose_asset.py
@@ -75,7 +75,7 @@ def fetch_asset_urls(response):
     # Find all elements with the specified class
     # elements = soup.find_all(class_="productDocuments")
     elements = soup.find_all('div', attrs={'lpos': "Downloads region area"})
-    elements = soup.find_all('div', class_='proCallToAction') + elements
+    elements = soup.find_all('div', class_='bose-cta--show') + elements
     tech_elements = soup.find_all(class_="productList") + soup.find_all(class_="productReference")
 
     # Extract href attributes
