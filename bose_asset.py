035415a86edbe62cb4a8e23e1bd140b6c9bcd177
Shajaz Hashim <shajaz.hashim@gapac.com>
2023-11-17 00:29:44 +0530

    download button added

--- bose_asset.py
+++ bose_asset.py
@@ -79,7 +79,9 @@ def fetch_asset_urls(response):
             
     # Find all elements with the specified class
     # elements = soup.find_all(class_="productDocuments")
-    elements = soup.find_all('div', attrs={'lpos': "Downloads region area"}) + soup.find_all('div', attrs={'lpos': "Download region area"})
+    download_button = soup.find_all('div', class_="buttonLink")
+    elements = soup.find_all('div', attrs={'lpos': "Downloads region area"}) + \
+        soup.find_all('div', attrs={'lpos': "Download region area"}) + download_button
     elements = soup.find_all('div', class_='bose-cta--show') + elements
     tech_elements = soup.find_all(class_="productList") + soup.find_all(class_="productReference")
 
@@ -92,6 +94,8 @@ def fetch_asset_urls(response):
     other_asset_text = []
     for element in elements:
         a_tags = element.find_all('a')
+        if element in download_button and 'Download' not in a_tags[1].text:
+            continue
         for asset in a_tags:
             if asset.get('href'):
                 if "assets.bose.com" in asset.get('href'):
