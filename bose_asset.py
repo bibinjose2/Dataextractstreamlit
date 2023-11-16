6f05c1e9cf58cdbb27294938c7711ae317d9c3db
Shajaz Hashim <shajaz.hashim@gapac.com>
2023-11-17 02:35:37 +0530

    a tag issue fixed

--- bose_asset.py
+++ bose_asset.py
@@ -106,8 +106,10 @@ def fetch_asset_urls(response):
         except:
             pass
         try:
-            if element in zip_download and 'Download' not in str(a_tags[0].text):
-                continue
+            if element in zip_download:
+                a_tags = zip_download
+                if 'Download' not in str(a_tags[0].text):
+                    continue
         except:
             pass
         for asset in a_tags:
